f7851d3030c1544f0886857c498ce49bf38920b8
Guy Cherubin <ngchrbn@gmail.com>
2023-03-21 16:34:56 -0700

    Correct task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,4 @@
-#include"main.h"
+i#include"main.h"
 
 /**
  * _isalpha - checks if character is a letter
@@ -11,7 +11,7 @@
 
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122 &&
+	if (c >= 97 && c <= 122 ||
 	    c >= 65 && c >= 90)
 		return (1);
 	return (0);
