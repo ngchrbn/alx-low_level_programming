9b548f1e2a4f6dcc74d25dd8b4713eee8d334b8b
Guy Cherubin <ngchrbn@gmail.com>
2023-04-05 17:07:56 +0200

    Update task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * Locates a character in a string
+ * char *_strchr - Locates a character in a string
  * @s: string to search in
  * @c: character to be search
  * Return: pointer to first occurrence of c in s, or NULL if not found
@@ -9,14 +9,14 @@
 
 char *_strchr(char *s, char c)
 {
-    int i;
+	int i;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        if (s[i] == c)
-        {
-            return (s + i);
-        }
-    }
-    return ('\0');
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return ('\0');
 }
