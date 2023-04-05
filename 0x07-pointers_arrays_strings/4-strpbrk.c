9d224a2ea6d7aaa452c11bc77a5c6fb28629f3e7
Guy Cherubin <ngchrbn@gmail.com>
2023-04-05 17:15:28 +0200

    Update task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * Searches a string for any of a set of bytes
+ * char *_strpbrk - Searches a string for any of a set of bytes
  * @s: string to be searched
  * @accept: string containing the characters to search for
  * Return: pointer to the byte in s that matches one of the bytes in accept,
@@ -10,9 +10,9 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-    int i, j;
+	int i, j;
 
-    i = 0;
+	i = 0;
 	while (s[i] != '\0')
 	{
 		j = 0;
