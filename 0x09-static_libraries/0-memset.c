6d8aca237d6f212e2b8bcf8fd89bb140ddbbe65e
Guy Cherubin <ngchrbn@gmail.com>
2023-04-10 12:15:30 +0200

    Add task 0

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * char *_memset - Fills memory with a constant byte
+ * @s: pointer for the memory area to be filled
+ * @b: constant byte
+ * @n: bytes of the memory area pointed to by s
+ * Return: pointer to memory area s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
