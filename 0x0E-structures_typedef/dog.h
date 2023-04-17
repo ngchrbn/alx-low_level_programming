4c21c9dd207fbf9ec305807e3e742a8e0a8ab16d
Guy Cherubin <ngchrbn@gmail.com>
2023-04-18 01:40:47 +0200

    Add task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+int _putchar(char c);
+
+/**
+ * struct dog - struct dog
+ * @name: name of dog.
+ * @age: age of dog.
+ * @owner: owner of dog.
+ *
+ * Description: struct dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
