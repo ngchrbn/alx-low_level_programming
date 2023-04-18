fe6cb7730f4077d82f2cc2e3665d1104ad4adff7
Guy Cherubin <ngchrbn@gmail.com>
2023-04-18 02:04:17 +0200

    Add task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 
+
 /**
  * struct dog - struct dog
  * @name: name of dog.
@@ -15,6 +16,11 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
