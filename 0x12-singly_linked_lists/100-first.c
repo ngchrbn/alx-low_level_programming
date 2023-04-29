983faec78fea9e857dec525833d21ebe484670d0
Guy Cherubin <ngchrbn@gmail.com>
2023-04-30 01:01:27 +0200

    Update advanced task 1

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,10 +5,10 @@
  * Return: void.
 */
 
-void first(void) __attribute__ ((constructor));
+void __attribute__ ((constructor)) first(void);
 
 void first(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+		   "I bore my house upon my back!\n");
 }
