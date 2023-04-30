56952c4fa70d028dea739e6bcea23af444d3895f
Guy Cherubin <ngchrbn@gmail.com>
2023-05-01 00:18:30 +0200

    Add task 0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,11 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -16,5 +21,6 @@ typedef struct listint_s
 } listint_t;
 
 int _putchar(char c);
+size_t print_listint(const listint_t *h);
 
 #endif /* LISTS_H */
