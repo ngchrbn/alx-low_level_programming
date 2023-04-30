7724e5ed15a79c300f548f8b255245a1da7e086e
Guy Cherubin <ngchrbn@gmail.com>
2023-05-01 00:42:34 +0200

    Add task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list.
+ * @head: pointer to the first element of the list.
+ * Return: void.
+*/
+
+void free_listint(listint_t *head)
+{
+	listint_t *current_node;
+
+	while (head != NULL)
+	{
+		current_node = head;
+		head = head->next;
+		free(current_node);
+	}
+}
