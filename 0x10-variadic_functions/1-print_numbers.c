07ff6a5f79d89566a7f8dd5042d506c98fe78b76
Guy Cherubin <ngchrbn@gmail.com>
2023-08-19 01:35:36 +0200

    0x10: Add print_numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - Prints numbers, followed by a new line
+ * @separator: string to be printed betwen numbers
+ * @n: number of integers passed to the function
+ * if separator is NULL, don't print it.
+ * Print a new line at the edn of your function.
+ * Returns void
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+
+		if (i < n - 1)
+			printf("%s", separator == NULL ? " " : separator);
+	}
+
+	printf("\n");
+	va_end(ap);
+}
