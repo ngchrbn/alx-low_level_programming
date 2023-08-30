80de7877fc28e7566ebce2ba79505b22ca990516
Guy Cherubin <ngchrbn@gmail.com>
2023-08-30 02:28:03 +0200

    0x13: Add task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Deletes the head node of a listint_t linked list.
+ * @head: Pointer to a pointer to the head of the list.
+ * Return: 0 if linked list is empty or head node's data (n).
+*/
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int n;
+
+	if (*head == NULL)
+		return (0);
+
+	temp = *head;
+	n = temp->n;
+	*head = temp->next;
+	free(temp);
+
+	return (n);
+}
