0e64ac0d8315bb2473f8982cc38c851ea9785535
Guy Cherubin <ngchrbn@gmail.com>
2023-08-30 03:29:06 +0200

    0x13: Update task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 	listint_t *new_node, *temp = *head;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new_node->n = n;
-		new_node->next = temp->next;
+		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
