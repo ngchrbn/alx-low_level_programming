d3750015adf24b724478c78a3be7b482098d7eff
Guy Cherubin <ngchrbn@gmail.com>
2024-02-18 19:42:15 +0200

    0x1A: Add task 2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Gives the index of a key
+ * @key: the key
+ * @size: size of the array of the hash table
+ * Uses djb2 algorithm for hashing
+ * Return: index at which the key/value pair should be stored
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash;
+
+	hash = hash_djb2(key);
+
+	return (hash % size);
+}
