cc2c87254d20927e2b303f950e36ffb496cc9f20
Guy Cherubin <ngchrbn@gmail.com>
2024-02-19 18:19:48 +0200

    0x1A: Add task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,77 @@
+#include "hash_tables.h"
+
+
+/**
+ * make_hash_node - creates a new hash node
+ * @key: key of the node
+ * @value: corresponding value of key
+ *
+ * Return: new node if success, otherwise NULL
+ */
+hash_node_t *make_hash_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (NULL);
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+
+	node->next = NULL;
+	return (node);
+}
+
+/**
+ * hash_table_set - Adds an element to the hash table.
+ * @ht: Hash table where to add the element.
+ * @key: key - Cannot be empty!
+ * @value: value associated with the key. Can be empty.
+ * In case of collision, new node is added at the beginning of the list.
+ * Return: 1 if success, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node, *tmp;
+	char *new_value;
+
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+			key == NULL || strlen(key) == 0 || value == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	tmp = ht->array[index];
+
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			new_value = strdup(value);
+			if (new_value == NULL)
+				return (0);
+			free(tmp->value);
+			tmp->value = new_value;
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+
+	new_node = make_hash_node(key, value);
+	if (new_node == NULL)
+		return (0);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
